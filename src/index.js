221532be12eb17bd164a1913ee9a083d6b65de58
willow <wms878@gmail.com>
2017-12-04 14:58:45 +0900

    installed firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from "firebase";
+import { Config } from './config.js';
+
+var config = {
+  apiKey: Config.firebaseApiKey,
+  authDomain: Config.firebaseAuthDomain,
+  databaseURL: Config.firebaseDatabaseURL,
+  projectId: Config.firebaseProjectId,
+  storageBucket: Config.firebaseStorageBucket,
+  messagingSenderId: Config.firebaseMessagingSenderId
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
